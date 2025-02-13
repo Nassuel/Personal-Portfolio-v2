26ec9d7ac0ff4eba3d900a887ff38bc702213c33
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-02-13 01:08:57 -0800

    Cleanup

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import NextLink from 'next/link'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.6} fontSize="md">
+    <Box align="center" opacity={0.6} fontSize="md" mt={{base: 'auto', lg: "auto" }}>
       &copy; {new Date().getFullYear()} Nassuel Valera Cuevas. All Rights
       Reserved | Scaffolding by{' '}
       <Link as={NextLink} href="https://www.craftz.dog" passHref target="_blank">
