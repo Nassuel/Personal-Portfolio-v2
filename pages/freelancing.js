f9c50092641bad72f2b1aa6f696db2675a25fd8c
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2025-06-10 17:34:09 -0700

    Update link for Parents for Safe Childcare project

--- pages/freelancing.js
+++ pages/freelancing.js
@@ -38,7 +38,7 @@ const Freelancing = () => (
           <WorkGridItem
             title="Parents for Safe Childcare"
             thumbnail={parentsForSafeChildcare}
-            link="parentsforsafechildcare.org"
+            link="parents-for-safe-childcare.vercel.app/"
           >
             Parents for safe childcare is a nonprofit organization I have volunteered
             to create a new and refreshred website for.
