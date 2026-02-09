1c6910969d4f6a65cd9e17aada3168f52ea1cfdf
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:04:29 -0800

    Reformatting components/layouts/article.js with prettier rules

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -1,37 +1,37 @@
-import { motion } from 'framer-motion'
-import Head from 'next/head'
-import { GridItemStyle } from '../grid-item'
+import { motion } from "framer-motion";
+import Head from "next/head";
+import { GridItemStyle } from "../grid-item";
 
 const variants = {
-  hidden: { opacity: 0, x: 0, y: 20 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: -0, y: 20 }
-}
+    hidden: { opacity: 0, x: 0, y: 20 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: -0, y: 20 }
+};
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - NVC`
-  return (
-    <motion.article
-      initial="hidden"
-      animate="enter"
-      exit="exit"
-      variants={variants}
-      transition={{ duration: 0.4, type: 'easeInOut' }}
-      style={{ position: 'relative', marginTop: '2rem' }}
-    >
-      <>
-        {title && (
-          <Head>
-            <title>{t}</title>
-            <meta property="og:title" content={t} />
-          </Head>
-        )}
-        {children}
+    const t = `${title} - NVC`;
+    return (
+        <motion.article
+            initial="hidden"
+            animate="enter"
+            exit="exit"
+            variants={variants}
+            transition={{ duration: 0.4, type: "easeInOut" }}
+            style={{ position: "relative", marginTop: "2rem" }}
+        >
+            <>
+                {title && (
+                    <Head>
+                        <title>{t}</title>
+                        <meta property="og:title" content={t} />
+                    </Head>
+                )}
+                {children}
 
-        <GridItemStyle />
-      </>
-    </motion.article>
-  )
-}
+                <GridItemStyle />
+            </>
+        </motion.article>
+    );
+};
 
-export default Layout
+export default Layout;
