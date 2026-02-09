dd07049b0fc2d5d21d76530d16a04a272996b184
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:07:58 -0800

    Reformat components/section.js with Prettier rules

--- components/section.js
+++ components/section.js
@@ -1,17 +1,17 @@
-import { motion } from 'framer-motion'
-import { Box } from '@chakra-ui/react'
+import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/react";
 
-const MotionBox = motion.create(Box)
+const MotionBox = motion.create(Box);
 
 const Section = ({ children, delay = 0 }) => (
-  <MotionBox
-    initial={{ y: 10, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.8, delay }}
-    mb={6}
-  >
-    {children}
-  </MotionBox>
-)
+    <MotionBox
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay }}
+        mb={6}
+    >
+        {children}
+    </MotionBox>
+);
 
-export default Section
+export default Section;
