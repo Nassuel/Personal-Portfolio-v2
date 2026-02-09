a0ddd1ff519d4d2a54b38c67627a5e45afdb10d8
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:04:48 -0800

    Reformatting components/chakra.js with prettier rules

--- components/chakra.js
+++ components/chakra.js
@@ -1,13 +1,15 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { ThemeProvider } from 'next-themes'
-import system from '../lib/theme'
+import { ChakraProvider } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import system from "../lib/theme";
 
 export default function Chakra({ children }) {
-  return (
-    <ThemeProvider attribute="class" disableTransitionOnChange defaultTheme="dark">
-      <ChakraProvider value={system}>
-        {children}
-      </ChakraProvider>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider
+            attribute="class"
+            disableTransitionOnChange
+            defaultTheme="dark"
+        >
+            <ChakraProvider value={system}>{children}</ChakraProvider>
+        </ThemeProvider>
+    );
 }
