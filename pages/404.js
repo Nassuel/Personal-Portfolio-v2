b5400e90e7009f8e1182813b0441ead18b13baa9
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:03:28 -0800

    Reformatting with prettier rules 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,33 +1,33 @@
-import NextLink from 'next/link'
+import NextLink from "next/link";
 import {
-  Box,
-  Heading,
-  Text,
-  Container,
-  Separator,
-  Button,
-  HStack
-} from '@chakra-ui/react'
-import { IoHomeSharp } from 'react-icons/io5'
+    Box,
+    Heading,
+    Text,
+    Container,
+    Separator,
+    Button,
+    HStack
+} from "@chakra-ui/react";
+import { IoHomeSharp } from "react-icons/io5";
 
 const NotFound = () => {
-  return (
-    <Container maxW="container.sm" py={10}>
-      <Heading as="h1">Not found</Heading>
-      <Text>The page you&apos;re looking for was not found.</Text>
-      <Separator my={6} />
-      <Box my={6} textAlign="center">
-        <Button asChild colorPalette="green">
-          <NextLink href="/">
-            <HStack gap={2}>
-              <IoHomeSharp />
-              <span>Return to home</span>
-            </HStack>
-          </NextLink>
-        </Button>
-      </Box>
-    </Container>
-  )
-}
+    return (
+        <Container maxW="container.sm" py={10}>
+            <Heading as="h1">Not found</Heading>
+            <Text>The page you&apos;re looking for was not found.</Text>
+            <Separator my={6} />
+            <Box my={6} textAlign="center">
+                <Button asChild colorPalette="green">
+                    <NextLink href="/">
+                        <HStack gap={2}>
+                            <IoHomeSharp />
+                            <span>Return to home</span>
+                        </HStack>
+                    </NextLink>
+                </Button>
+            </Box>
+        </Container>
+    );
+};
 
-export default NotFound
+export default NotFound;
