c6c572296a73b85444f0d3a501069458f5ee42b6
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:04:43 -0800

    Reformatting components/bio.js with prettier rules

--- components/bio.js
+++ components/bio.js
@@ -1,19 +1,19 @@
-import { chakra } from '@chakra-ui/react'
+import { chakra } from "@chakra-ui/react";
 
-export const BioSection = chakra('div', {
-  base: {
-    paddingLeft: '3.4em',
-    textIndent: '-3.4em',
-    marginBottom: '0.5em',
-    '& a': {
-      display: 'inline',
+export const BioSection = chakra("div", {
+    base: {
+        paddingLeft: "3.4em",
+        textIndent: "-3.4em",
+        marginBottom: "0.5em",
+        "& a": {
+            display: "inline"
+        }
     }
-  }
-})
+});
 
-export const BioYear = chakra('span', {
-  base: {
-    fontWeight: 'bold',
-    marginRight: '1em',
-  }
-})
+export const BioYear = chakra("span", {
+    base: {
+        fontWeight: "bold",
+        marginRight: "1em"
+    }
+});
