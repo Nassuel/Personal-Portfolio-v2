24e15c9bc3698dfdf9f446f2d12e01810808f82f
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:06:40 -0800

    Reformatting components/logo.js with prettier rules

--- components/logo.js
+++ components/logo.js
@@ -1,54 +1,54 @@
-import Link from 'next/link'
-import { Text, chakra } from '@chakra-ui/react'
-import { useTheme } from 'next-themes'
-import { useEffect, useState } from 'react'
+import Link from "next/link";
+import { Text, chakra } from "@chakra-ui/react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
-const LogoBox = chakra('span', {
-  base: {
-    fontWeight: 'normal',
-    fontSize: '40px',
-    display: 'inline-flex',
-    alignItems: 'center',
-    height: '30px',
-    lineHeight: '20px',
-    padding: '10px',
-    '& > svg': {
-      transition: '200ms ease'
-    },
-    '&:hover > svg': {
-      transform: 'rotate(20deg)'
+const LogoBox = chakra("span", {
+    base: {
+        fontWeight: "normal",
+        fontSize: "40px",
+        display: "inline-flex",
+        alignItems: "center",
+        height: "30px",
+        lineHeight: "20px",
+        padding: "10px",
+        "& > svg": {
+            transition: "200ms ease"
+        },
+        "&:hover > svg": {
+            transform: "rotate(20deg)"
+        }
     }
-  }
-})
+});
 
 const Logo = () => {
-  const { resolvedTheme } = useTheme()
-  const [mounted, setMounted] = useState(false)
+    const { resolvedTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
-  // Avoid hydration mismatch
-  const textColor = !mounted 
-    ? 'gray.800' 
-    : resolvedTheme === 'dark' 
-      ? 'whiteAlpha.900' 
-      : 'gray.800'
+    // Avoid hydration mismatch
+    const textColor = !mounted
+        ? "gray.800"
+        : resolvedTheme === "dark"
+          ? "whiteAlpha.900"
+          : "gray.800";
 
-  return (
-    <Link href="/" scroll={false}>
-      <LogoBox>
-        <Text
-          color={textColor}
-          fontFamily="Babylonica"
-          fontWeight="normal"
-        >
-          Nassuel Valera Cuevas
-        </Text>
-      </LogoBox>
-    </Link>
-  )
-}
+    return (
+        <Link href="/" scroll={false}>
+            <LogoBox>
+                <Text
+                    color={textColor}
+                    fontFamily="Babylonica"
+                    fontWeight="normal"
+                >
+                    Nassuel Valera Cuevas
+                </Text>
+            </LogoBox>
+        </Link>
+    );
+};
 
-export default Logo
+export default Logo;
