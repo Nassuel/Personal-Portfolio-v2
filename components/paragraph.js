beb2a0a5a25c006debddf68833b84d9a70f51811
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-02-08 21:07:15 -0800

    Reformatting components/paragraph.js with prettier rules

--- components/paragraph.js
+++ components/paragraph.js
@@ -1,21 +1,21 @@
-import { chakra } from '@chakra-ui/react'
+import { chakra } from "@chakra-ui/react";
 
-const Paragraph = (props) => (
-  <chakra.p
-    textAlign="justify"
-    textIndent="1em"
-    css={{ hyphens: 'auto' }}
-    {...props}
-  />
-)
+const Paragraph = props => (
+    <chakra.p
+        textAlign="justify"
+        textIndent="1em"
+        css={{ hyphens: "auto" }}
+        {...props}
+    />
+);
 
-const ParagraphClean = (props) => (
-  <chakra.p
-    textAlign="justify"
-    textIndent="0.05em"
-    css={{ hyphens: 'auto' }}
-    {...props}
-  />
-)
+const ParagraphClean = props => (
+    <chakra.p
+        textAlign="justify"
+        textIndent="0.05em"
+        css={{ hyphens: "auto" }}
+        {...props}
+    />
+);
 
-export { Paragraph, ParagraphClean }
+export { Paragraph, ParagraphClean };
