c0c08ab89d4e47855c2902392cd37067fb18877b
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-03-24 00:32:01 -0700

    Add `prettier.config.ts` with code formatting rules (prettier.config.ts)

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+    arrowParens: "avoid",
+    singleQuote: false,
+    bracketSpacing: true,
+    endOfLine: "lf",
+    semi: true,
+    tabWidth: 4,
+    trailingComma: "none"
+};
+
+export default config;
