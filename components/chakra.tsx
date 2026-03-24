e54bbcf05808e79101101a82aac130ac8e1c161d
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-03-24 00:52:23 -0700

    Update `children` type and refine imports in `components/chakra.tsx`

--- components/chakra.tsx
+++ components/chakra.tsx
@@ -1,9 +1,10 @@
+import type { ReactNode } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThemeProvider } from "next-themes";
 import system from "../lib/theme";
 
 interface ChakraProps {
-    children: React.ReactNode;
+    children: ReactNode;
 }
 
 export default function Chakra({ children }: ChakraProps) {
