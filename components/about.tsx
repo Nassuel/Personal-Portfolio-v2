aceb665c5571d1e5f511d9a86ef736c3d8717679
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-03-24 00:29:50 -0700

    Add `AboutSection` and `AboutYear` components with Chakra styling (components/about.tsx)

---
+++ components/about.tsx
@@ -0,0 +1,19 @@
+import { chakra } from "@chakra-ui/react";
+
+export const AboutSection = chakra("div", {
+    base: {
+        paddingLeft: "3.4em",
+        textIndent: "-3.4em",
+        marginBottom: "0.5em",
+        "& a": {
+            display: "inline"
+        }
+    }
+});
+
+export const AboutYear = chakra("span", {
+    base: {
+        fontWeight: "bold",
+        marginRight: "1em"
+    }
+});
