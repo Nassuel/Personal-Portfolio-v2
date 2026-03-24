8e582027a0c650f9547683b2b2febeddb9c03e48
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-03-24 00:30:48 -0700

    Add `loadGLTFModel` utility for loading 3D models with shadows (lib/model.ts)

---
+++ lib/model.ts
@@ -0,0 +1,49 @@
+import * as THREE from "three";
+import { GLTFLoader, type GLTF } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+
+const draco = new DRACOLoader();
+draco.setDecoderConfig({ type: "js" });
+draco.setDecoderPath("https://www.gstatic.com/draco/v1/decoders/");
+
+interface LoadGLTFModelOptions {
+    receiveShadow: boolean;
+    castShadow: boolean;
+}
+
+export function loadGLTFModel(
+    scene: THREE.Scene,
+    glbPath: string,
+    options: LoadGLTFModelOptions = { receiveShadow: true, castShadow: true }
+): Promise<THREE.Object3D> {
+    const { receiveShadow, castShadow } = options;
+    return new Promise((resolve, reject) => {
+        const loader = new GLTFLoader();
+        loader.setDRACOLoader(draco);
+
+        loader.load(
+            glbPath,
+            (gltf: GLTF) => {
+                const obj = gltf.scene;
+                obj.name = "dog";
+                obj.position.y = 0;
+                obj.position.x = 0;
+                obj.receiveShadow = receiveShadow;
+                obj.castShadow = castShadow;
+                scene.add(obj);
+
+                obj.traverse(function (child: THREE.Object3D) {
+                    if ((child as THREE.Mesh).isMesh) {
+                        (child as THREE.Mesh).castShadow = castShadow;
+                        (child as THREE.Mesh).receiveShadow = receiveShadow;
+                    }
+                });
+                resolve(obj);
+            },
+            undefined,
+            (error: unknown) => {
+                reject(error);
+            }
+        );
+    });
+}
