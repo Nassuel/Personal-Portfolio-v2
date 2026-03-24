16faacdd0bfe0c6386b0c560de27030eefa9f499
Nassuel Valera Cuevas <valeran1@seattleu.edu>
2026-03-24 00:30:42 -0700

    Add `Main` layout component for page structure with meta tags and Chakra UI (components/layouts/main.tsx)

---
+++ components/layouts/main.tsx
@@ -0,0 +1,48 @@
+import Head from "next/head";
+import NavBar from "../navbar";
+import { Box, Container } from "@chakra-ui/react";
+import Footer from "../footer";
+import type { NextRouter } from "next/router";
+
+interface MainProps {
+    children: React.ReactNode;
+    router: NextRouter;
+}
+
+const Main = ({ children, router }: MainProps) => {
+    return (
+        <Box as="main">
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta name="description" content="Nassuel's homepage" />
+                <meta name="author" content="Nassuel Valera Cuevas" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link
+                    rel="shortcut icon"
+                    href="/favicon.ico"
+                    type="image/x-icon"
+                />
+                <meta property="og:site_name" content="Nassuel Valera Cuevas" />
+                <meta name="og:title" content="NVC" />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:image"
+                    content="https://www.nassuel.com/card.jpg"
+                />
+                <title>NVC - Home</title>
+            </Head>
+
+            <NavBar path={router.asPath} />
+
+            <Container maxW="4xl" pt={8}>
+                {children}
+                <Footer />
+            </Container>
+        </Box>
+    );
+};
+
+export default Main;
